323931aa0378353e0b39a757a9389cd6996fa4ef
Dmitriy Manuylov <dmitry.seryozniy@yandex.ru>
2023-03-20 00:28:06 +0300

    add random

--- Еxample003_Sum/Program.cs
+++ Еxample003_Sum/Program.cs
@@ -1,3 +1,6 @@
-﻿int numberA = 3;
-int numberВ = 5;
-Console.WriteLine(numberA + numberВ );
+﻿Int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+Int numberB = new Random().Next(1,10);
+Console.WriteLine(numberВ);
+Int result = numberA + numberB;
+Console.WriteLine(result);
