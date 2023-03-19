edd4a878fe4d71997b428e864e9586db20de7ca8
Dmitriy Manuylov <dmitry.seryozniy@yandex.ru>
2023-03-20 01:36:44 +0300

    Еxample007_Magic

---
+++ Еxample007_Magic/Program.cs
@@ -0,0 +1,36 @@
+﻿
+ Console.Clear();
+
+int xa = 1; int ya = 1;
+int xb = 1; int yb = 30;
+int xc = 40; int yc = 30;
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("a");
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("b");
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("c");
+int x = xa, y = xb;
+int count = 0;
+while (count < 10000)
+{
+    int what = new Random().Next(0, 3);
+        if (what == 0)
+            {
+                x = (x + xa) / 2;
+                y = (y + ya) / 2;
+            }
+        if (what == 1)
+            {
+                x = (x + xb) / 2;
+                y = (y + yb) / 2;
+            }
+        if (what == 2)
+            {
+                x = (x + xс) / 2;
+                y = (y + yс) / 2;
+            }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+}
